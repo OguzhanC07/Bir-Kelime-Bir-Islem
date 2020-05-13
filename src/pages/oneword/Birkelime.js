536299de0631837a42bc7c9e7cf065eccc954329
Oguzhan Can <oguzhancan2009@gmail.com>
2020-05-14 00:06:44 +0300

    Word Finder
    Word Finder

--- src/pages/oneword/Birkelime.js
+++ src/pages/oneword/Birkelime.js
@@ -5,23 +5,23 @@ import TextFileReader from './TextFileReader';
 
 var myTxt=require("./deneme.txt");
 var letters=[];
-
 export default class Birkelime extends Component {
     state={
         howmany:0,
         stateletters:[],
-        isVisible:true
+        isVisible:true,
+        sendbutton:false
     }
     
     
     getRandomLetters=(e)=>{
         var emptyString = "";
-        var sessiz = "bçcdfgğhjklmnpqrsştvyz";
+        
+        var sessiz = "bçcdfgğhjklmnprsştvyz";
         var sesli="aeıioöuü"
-
+        
         emptyString=sessiz[Math.floor(Math.random()*sessiz.length)];
         letters[this.state.howmany]=emptyString;
-
         if(this.state.howmany>=4){
             emptyString=sesli[Math.floor(Math.random()*sesli.length)];
             letters[this.state.howmany]=emptyString;
@@ -38,26 +38,40 @@ export default class Birkelime extends Component {
             })
         }
     }
+    sendLetters=(e)=>{
+        this.setState({
+            sendbutton:true
+        })
+    }
 
     render() {
         return (
             <div>
-                 <TextFileReader
-                        txt={myTxt}
-                />
+              
+                
+              
                 <header className="App-header">
-                {
+                {   
+                    
                         <ul className="menu">
                             {
                                     this.state.stateletters.map((value, index) => {
                                     return <li key={index}>{index + 1}. harf ={value}</li>
                                 })}
                         </ul>
+
+                     
                 }
 
-                
+
+                        <TextFileReader
+                            txt={myTxt}
+                            letter={this.state.stateletters}
+                            validation={this.state.sendbutton}
+                        />    
+                  
                     <Button disabled={this.state.isVisible ? "" : "{true}"} onClick={this.getRandomLetters.bind(this)}>Rastgele Harf Getir.</Button>
-                    <Button disabled={this.state.isVisible ? "{true}" : ""}> Sonucu Bul</Button>
+                    <Button disabled={this.state.isVisible ? "{true}" : ""} onClick={this.sendLetters}> Sonucu Bul</Button>
                 </header> 
                    
             </div>
--- src/pages/oneword/TextFileReader.js
+++ src/pages/oneword/TextFileReader.js
@@ -1,9 +1,10 @@
 import React from "react";
+//import _ from "underscore";
 
-
-class TextFileReader extends React.Component {
-    state={
-        text:""
+class TextFileReader extends React.Component {	
+	state={
+		text:"",
+		statewords:[0]
     }
 
 	componentDidMount() {
@@ -26,21 +27,118 @@ class TextFileReader extends React.Component {
 		rawFile.send(null);
 	};
 
+
+	/*
+		var alphabet=["a","b","c","ç","d","e","f","g","ğ","h","ı","i","j","k","l","m","n","o","ö","p","r","s","ş","t","u","ü","v","y","z"];
+			var wordmap=[],lettermap=[];
+			var wordslist=[];
+			var trials=0;
+			for(var i=0;i<alphabet.length;i++){
+				lettermap[i]=0;
+				wordmap[i]=0;
+			}
+			
+			for(i=0;i<word.length;i++){
+				for(var k=0;k<alphabet.length;k++){
+					if(word[i]===alphabet[k]){
+						wordmap[k]++;
+					}
+				}
+			}
+
+			for(i=0;i<this.props.letter.length;i++){
+				for(k=0;k<alphabet.length;k++){
+					if(this.props.letter[i]===alphabet[k]){
+						lettermap[k]++;
+					}
+				}
+			}
+			
+			var bulundu=0;
+			for(i=0;i<alphabet.length;i++){
+				if(lettermap[i]===2 && wordmap[i]===2){
+					bulundu=bulundu+2;
+				}
+				else if(lettermap[i]===1 && wordmap[i]===1){
+					bulundu=bulundu+1;
+				}
+				else if(lettermap[i]===3 && wordmap[i]===3){
+					bulundu=bulundu+3;
+				}
+			}
+
+			if(bulundu===word.length){
+				wordslist.push(word);
+			}
+			trials++;
+			console.log(trials);
+	
+	
+	
+	*/
+	compWords=(word)=>{	
+		if(this.props.validation===true){
+			if(word[0]===this.props.letter[0] || word[0]===this.props.letter[1] || word[0]===this.props.letter[2] || word[0]===this.props.letter[3] 
+				|| word[0]===this.props.letter[4] || word[0]===this.props.letter[5] || word[0]===this.props.letter[6] || word[0]===this.props.letter[7]){
+				var alphabet=["a","b","c","ç","d","e","f","g","ğ","h","ı","i","j","k","l","m","n","o","ö","p","r","s","ş","t","u","ü","v","y","z"];
+				var wordmap=[],lettermap=[];
+				var wordslist=[];
+				var trials=0;
+				for(var i=0;i<alphabet.length;i++){
+					lettermap[i]=0;
+					wordmap[i]=0;
+				}
+				
+				for(i=0;i<word.length;i++){
+					for(var k=0;k<alphabet.length;k++){
+						if(word[i]===alphabet[k]){
+							wordmap[k]++;
+						}
+					}
+				}
+
+				for(i=0;i<this.props.letter.length;i++){
+					for(k=0;k<alphabet.length;k++){
+						if(this.props.letter[i]===alphabet[k]){
+							lettermap[k]++;
+						}
+					}
+				}
+				
+				var bulundu=0;
+				for(i=0;i<alphabet.length;i++){
+					if(lettermap[i]===2 && wordmap[i]===2){
+						bulundu=bulundu+2;
+					}
+					else if(lettermap[i]===1 && wordmap[i]===1){
+						bulundu=bulundu+1;
+					}
+					else if(lettermap[i]===3 && wordmap[i]===3){
+						bulundu=bulundu+3;
+					}
+				}
+
+				if(bulundu===word.length && word.length>3){
+					wordslist.push(word);
+					this.setState({
+						statewords:wordslist
+					})
+				}
+				trials++;
+				console.log(trials);
+			}
+		}
+	}
 	render() {
 		return (
 			<div>
-				{this.state.text.split(',').map((item, key)  => {
-						
-						if(item.toString()==="aba güreşi")
-						return <span key={key}>{item}<br /></span>;
-						else return null;
-				})}
-                
-
-                {
-                   
-                }
-
+				{
+					this.state.text.split(',').map((item,key)=>{
+						if(key<100000){
+							this.compWords(item);
+						}
+					})
+				}
 			</div>
 		);
 	}
